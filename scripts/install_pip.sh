40f38df2bbd4236ca4f9b1de590a10da28fd38e9
Andri Horat <horatan@student.ethz.ch>
2023-11-28 10:00:15 +0100

    install denoising_diffusion_pytorch

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -10,8 +10,5 @@ echo "Activating virtual environment"
 source /cluster/scratch/$USER/MARS/bin/activate
 /cluster/scratch/$USER/MARS/bin/pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 /cluster/scratch/$USER/MARS/bin/pip install -r requirements.txt
-
-git clone git@github.com:qubvel/segmentation_models.pytorch.git
-cd segmentation_models.pytorch
 /cluster/scratch/$USER/MARS/bin/pip install -e .
-cd ..
+/cluster/scratch/$USER/MARS/bin/pip install denoising_diffusion_pytorch
